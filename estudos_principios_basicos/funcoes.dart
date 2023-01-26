a98d25f4238bfe7ad836b0aece5841068db8885f
Victor Santana <103072762+victorhosantana@users.noreply.github.com>
2023-01-26 20:31:39 +0000

    Estudos de funções

--- estudos_principios_basicos/funcoes.dart
+++ estudos_principios_basicos/funcoes.dart
@@ -9,16 +9,40 @@ bool funcMaiorDeIdade(int idade) {
   }
 }
 
+/* Ao especificar o tipo antes do nome da função, este será o retorno da função
+  Se não especificar nada ou especificar 'void', a função retornará 'vazio' */
+
+/* 
+  TIPOS DE PARÂMETROS NAS CHAMADAS DE FUNÇÕES
+  1 - Posicionais Obrigatórios
+  2 - Nomeados Opcionais
+  3 - Parâmetros "Padrão"
+  4 - Modificador "required"
+ */
+
+// Função com parâmetros Posicionais Obrigatórios
+void funcMaiorIdadeComTexto(String? nomeDaPessoa, int? idade) {
+  if (idade != null) {
+  if (idade >= 18) {
+    print("$nomeDaPessoa, você é maior de idade!");
+  } else {
+    print("$nomeDaPessoa, você NÃO é maior de idade!");
+  }
+}}
+
 void main() {
   print("\nInforme o seu nome: ");
   String? nome = stdin.readLineSync();
   print("\nInforme a sua idade: ");
   String? idadeEntrada = stdin.readLineSync();
-  int anos;
+  int? anos;
   if (idadeEntrada != null) {
     anos = int.parse(idadeEntrada);
     print(
-      "Olá, $nome! Como a sua idade é $anos, o comparativo se você é maior de idade é: ${funcMaiorDeIdade(anos)}");
+        "Olá, $nome! Como a sua idade é $anos, o comparativo se você é maior de idade é: ${funcMaiorDeIdade(anos)}");
   }
 
+  print("\nOutras chamadas de funções\n");
+  funcMaiorIdadeComTexto(nome, anos);
+  funcMaiorIdadeComTexto("Fulano", 17);
 }
