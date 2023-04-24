0a3784abd9b914fbf9ee4b52a5697e1cbfbbd3df
Victor Santana <103072762+victorhosantana@users.noreply.github.com>
2023-04-24 13:09:28 +0100

    Estudo colecoes

--- estudos_principios_basicos/colecoes.dart
+++ estudos_principios_basicos/colecoes.dart
@@ -15,6 +15,24 @@ void main() {
   print("As cidades visitadas são:\n$cidadesVisitadas");
   print(cidadesVisitadas.isNotEmpty);
   print(cidadesVisitadas.length);
+
+// Maps são coleções Chave:Valor. Também conhecido como "Dicionários" em outras linguagens
+  Map<String, dynamic> registrarPrecos = {};
+
+  registrarPrecos["Recife"] = 1100;
+  registrarPrecos["Lisboa"] = 1750.59;
+  registrarPrecos["Chile"] = 1509.99;
+  registrarPrecos["Recife"] = 900; // Atualização de valores no Map
+  registrarPrecos["São Paulo"] = 1000;
+  registrarPrecos.remove("São Paulo");
+
+  print("\n-----\n");
+  print(registrarPrecos);
+  print(registrarPrecos.runtimeType);
+  print(registrarPrecos.isEmpty);
+  print(registrarPrecos.length);
+  print(registrarPrecos.keys);
+  print(registrarPrecos.values);
 }
 
 Set<String> registrarCidades(String destino, Set<String> preencheBancodeDados) {
