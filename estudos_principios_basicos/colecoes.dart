34cfccb5312a669c0ffa623ff66d652f73a031f8
Victor Santana <103072762+victorhosantana@users.noreply.github.com>
2023-05-02 21:56:46 +0100

    Estudos de Maps

--- estudos_principios_basicos/colecoes.dart
+++ estudos_principios_basicos/colecoes.dart
@@ -33,9 +33,42 @@ void main() {
   print(registrarPrecos.length);
   print(registrarPrecos.keys);
   print(registrarPrecos.values);
+
+  print("\n-----\n");
+  print("Utilização do Map como método de uma Classe");
+  Humano humano01 = Humano("Fulano", 27, true);
+  Humano humano02 = Humano("Beltrano", 32, false);
+  print(
+      "A primeira instância da Classe Humano é: ${humano01.nome}, ${humano01.idade} e ${humano01.contaCriada}.");
+  print(
+      "A segunda instância da Classe Humano é: ${humano02.nome}, ${humano02.idade} e ${humano02.contaCriada}.");
+  print("\nConversão em Map\n");
+  Map<String, dynamic> novoHumano = humano01.toMap();
+  print(novoHumano);
+  print(novoHumano.runtimeType);
+  print(novoHumano.length);
+  print(novoHumano.keys);
+  print(novoHumano.values);
 }
 
 Set<String> registrarCidades(String destino, Set<String> preencheBancodeDados) {
   preencheBancodeDados.add(destino);
   return preencheBancodeDados;
+}
+
+// Criação de uma classe com um método para converter as propriedades em um Map
+class Humano {
+  String nome;
+  int idade;
+  bool contaCriada;
+
+  Humano(this.nome, this.idade, this.contaCriada);
+
+  Map<String, dynamic> toMap() {
+    return {
+      "nome": this.nome,
+      "idade": this.idade,
+      "contaCriada": this.contaCriada
+    };
+  }
 }
